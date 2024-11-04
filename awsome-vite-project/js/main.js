3cb5155fad6f243b7b3bd5c7189fe428a5feab6d
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2024-11-04 13:57:59 -0500

    list

--- awsome-vite-project/js/main.js
+++ awsome-vite-project/js/main.js
@@ -1,1 +1,16 @@
 import "../CSS/style.css";
+const DOMSelectors = {
+  btn: document.querySelector(".change"),
+};
+
+DOMSelectors.btn.addEventListener("click", function () {
+  if (document.body.classList.contains("basic")) {
+    document.body.classList.add("water");
+    document.body.classList.remove("basic");
+    DOMSelectors.btn.innerHTML = "Make this website TERRIBLE again";
+  } else {
+    document.body.classList.add("basic");
+    document.body.classList.remove("water");
+    DOMSelectors.btn.innerHTML = "Make this website GREAT again";
+  }
+});
