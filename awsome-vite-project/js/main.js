601f91bee6470ed37c61e73074418283ddbdc901
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2024-11-11 12:00:04 -0500

    yg9

--- awsome-vite-project/js/main.js
+++ awsome-vite-project/js/main.js
@@ -1,21 +1,13 @@
 import "../CSS/style.css";
 import { pokemonStarters } from "./pokemon.js";
 const DOMSelectors = {
-  btn: document.querySelector(".sort-type"),
+  btn: document.querySelector(".bruh"),
   container: document.querySelector(".container"),
+  form: document.querySelector(".form"),
+  dropdown: document.querySelector(".types"),
 };
 
-DOMSelectors.btn.addEventListener("click", function () {
-  if (document.body.classList.contains("basic")) {
-    document.body.classList.add("water");
-    document.body.classList.remove("basic");
-    DOMSelectors.btn.innerHTML = "Make this website TERRIBLE again";
-  } else {
-    document.body.classList.add("basic");
-    document.body.classList.remove("water");
-    DOMSelectors.btn.innerHTML = "Make this website GREAT again";
-  }
-});
+
 
 function addcards(array) {
   array.forEach((item) =>
@@ -34,4 +26,45 @@ function addcards(array) {
   );
 }
 
+
 addcards(pokemonStarters);
+
+DOMSelectors.form.addEventListener("submit", function (event) {
+  let selectedtypes = DOMSelectors.dropdown.value;
+  let currentclass = document.body.classList.value;
+  event.preventDefault();
+  if(selectedtypes === "fire"){
+    
+    document.body.classList.remove(currentclass);
+    document.body.classList.add("fuego");
+    
+
+  }else if(selectedtypes === "water"){
+    
+    document.body.classList.remove(currentclass);
+    document.body.classList.add("aqua");
+    //if ti was previouisly water, it deletes water thats hwy it goes white
+    
+
+  }else if(selectedtypes === "grass"){
+    
+    document.body.classList.remove(currentclass);
+    document.body.classList.add("leaf");
+    
+
+  }
+  
+})
+
+
+// DOMSelectors.btn.addEventListener("click", function () {
+//   if (document.body.classList.contains("basic")) {
+//     document.body.classList.add("water");
+//     document.body.classList.remove("basic");
+//     DOMSelectors.btn.innerHTML = "Make this website TERRIBLE again";
+//   } else {
+//     document.body.classList.add("basic");
+//     document.body.classList.remove("water");
+//     DOMSelectors.btn.innerHTML = "Make this website GREAT again";
+//   }
+// });
